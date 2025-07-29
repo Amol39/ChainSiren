34e03a10c532716ff0e8a590fc9c0a302d5a4f49
Amol39 <amolchavan474004@gmail.com>
2025-07-29 19:05:03 +0530

    updated file

--- frontend/src/components/CreateAccountDetailsPage.js
+++ frontend/src/components/CreateAccountDetailsPage.js
@@ -24,6 +24,7 @@ export default function CreateAccountDetailsPage() {
 
   const isPasswordValid =
     password.length >= 8 && /[A-Z]/.test(password) && /\d/.test(password);
+    
 
   const getPasswordStrength = (pwd) => {
     let score = 0;
